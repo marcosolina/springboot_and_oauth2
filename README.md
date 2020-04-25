30b35e69ddb3a44aac7be80d2a3639ec90e0885e
marco <marco@192.168.1.8>
2020-04-25 16:07:52 +0200

    preparing projects

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # springoauthexperiments
 In this repo I will play with Spring Security and Oauth
+
+![Diagram](./misc/img/diagram.png)
---
+++ webappclient-service/src/test/java/com/marco/webappclientservice/WebappclientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marco.webappclientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappclientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
